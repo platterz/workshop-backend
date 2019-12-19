2ba8e500da7ec9a30e2622f44191161852371ea8
Yaroslav Taben <yaroslav.taben@gmail.com>
2019-12-19 14:23:23 -0500

    Add model factories

---
+++ spec/factories/restaurant_products.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: restaurant_products
+#
+#  id             :bigint           not null, primary key
+#  item           :string           not null
+#  price          :float            not null
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#  restaurants_id :bigint           not null
+#
+# Indexes
+#
+#  index_restaurant_products_on_restaurants_id  (restaurants_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (restaurants_id => restaurants.id)
+#
+
+
+require 'faker'
+
+FactoryBot.define do
+  factory :restaurant_product do
+    item { Faker::Hipster.word }
+    price { Faker::Number.decimal(5, 2) }
+    association :restaurant, strategy: :build
+  end
+end
---
+++ spec/factories/restaurants.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: restaurants
+#
+#  id                :bigint           not null, primary key
+#  description       :string
+#  image             :string
+#  image_description :string
+#  name              :string           not null
+#  price_range       :integer
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#
+
+
+require 'faker'
+
+FactoryBot.define do
+  factory :restaurant do
+    name { Faker::Hipster.word }
+    price_range { Faker::Number.between(1, 3) }
+
+    transient do
+      products_count { 0 }
+    end
+
+    trait :with_products do
+      products_count { 1 }
+    end
+
+    after(:build) do |restaurant, e|
+      restaurant.restaurant_products = build_list(:restaurant_product, e.products_count)
+    end
+  end
+end
