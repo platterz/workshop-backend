796ff290b7842d8e59e20b33897ece685727175d
Yaroslav Taben <yaroslav.taben@gmail.com>
2020-01-02 10:56:56 -0500

    Use Float for money type

--- app/graphql/types/restaurant_product_type.rb
+++ app/graphql/types/restaurant_product_type.rb
@@ -3,6 +3,6 @@
 class Types::RestaurantProductType < Types::BaseObject
   field :id, ID, null: false
   field :item, String, null: false
-  field :price, Numeric, null: false
+  field :price, Float, null: false
   field :restaurant, Types::RestaurantType, null: false
 end
