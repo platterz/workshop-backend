dbd6c78e4b234f6133bcc25c9d3b45acaa9d80ec
Yaroslav Taben <yaroslav.taben@gmail.com>
2020-01-02 10:57:46 -0500

    Fix typo in restaurants query type

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -5,8 +5,8 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
-    field :restaurant, resolver: Queries::Restaurant
-    field :restaurants, resolver: Queries::RestaurantQuery
+    field :restaurant, resolver: Queries::RestaurantQuery
+    field :restaurants, resolver: Queries::RestaurantsQuery
 
     field :restaurant_product, resolver: Queries::RestaurantProductQuery
   end
