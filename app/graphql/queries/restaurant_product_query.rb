130876475fac8783df5fc99a041daa11cddb5ef9
Yaroslav Taben <yaroslav.taben@gmail.com>
2020-01-02 10:55:44 -0500

    Fix restaurant product query typing

--- app/graphql/queries/restaurant_product_query.rb
+++ app/graphql/queries/restaurant_product_query.rb
@@ -1,12 +1,11 @@
 # frozen_string_literal: true
 
 class Queries::RestaurantProductQuery < Queries::BaseQuery
-  field :restaurant_product, Types::RestaurantProductType, null: false
-  field :errors, [Types::ValidationErrorType], null: false
+  type Types::RestaurantProductType, null: false
 
   argument :id, ID, required: false
 
   def resolve(id:)
-    { restaurant_product: RestaurantProduct.find(id), errors: [] }
+    RestaurantProduct.find(id)
   end
 end
